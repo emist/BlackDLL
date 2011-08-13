059af305af37f527844de06f3bc795bb537e3874
emistz <emistz@gmail.com>
2011-08-13 19:35:28 -0400

    More loggin options

--- Black/Logger.cpp
+++ Black/Logger.cpp
@@ -8,37 +8,45 @@ using namespace std;
 
 void Logger::elog(char * message)
 {
+/*
 	ofstream myfile;
 	myfile.open("C:\\Users\\emist\\log.txt", fstream::app);
 	myfile << message << endl;
 	myfile.flush();
 	myfile.close();
+*/
 }
 
 void Logger::elog(LPCWSTR message)
 {
+/*
 	wofstream myfile;
 	myfile.open("C:\\Users\\emist\\log.txt", fstream::app);
 	myfile << message << endl;
 	myfile.flush();
 	myfile.close();
+*/
 }
 
 void Logger::elog(LPCSTR * message)
 {
+/*
 	ofstream myfile;
 	myfile.open("C:\\Users\\emist\\log.txt", fstream::app);
 	myfile << *message << endl;
 	myfile.flush();
 	myfile.close();
+*/
 }
 
 void Logger::elog(string message)
 {
+/*
 	ofstream myfile;
 	myfile.open("C:\\Users\\emist\\log.txt", fstream::app);
 	myfile << message << endl;
 	myfile.flush();
 	myfile.close();
+*/
 }
 
